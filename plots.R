0fbed7f0e1f65a5a56cc20c490335cd3176729f8
Caitlyn Chitwood <caitlynlchambers@gmail.com>
2022-12-27 16:16:16 -0600

    Renaming data processing and plotting files. Changing viz file to R script

---
+++ plots.R
@@ -0,0 +1,29 @@
+###############################################################################
+#                         PLOTTING SCRIPT
+# This script contains functions used for visualization and analysis.
+# These functions are sourced by the master script.
+###############################################################################
+
+
+# Density Plots -----------------------------------------------------------
+#Density plot function
+plot_mult_density <- function(variable, data1, data2, data3=NULL){
+  kd <- density(data1) #define kernel density
+  plot(kd,
+       col='blue',
+       lwd=2,
+       main = variable) #create kernel density plot
+  kd2 <- density(data2)
+  lines(kd2,
+        col='red',
+        lwd=2)
+  if(!is.null(data3)){
+    kd3 <- density(data3)
+    lines(kd3,
+          col='green',
+          lwd=2)
+  }
+}
+
+# Histograms --------------------------------------------------------------
+
